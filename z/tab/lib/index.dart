a4aaeafbb77ecb78eb2996577babe08a3911198c
Jango <me.jango@protonmail.com>
2025-06-01 01:30:14 -0300

    tab is in

---
+++ z/tab/lib/index.dart
@@ -0,0 +1,2 @@
+export 'src/tab.dart';
+export 'src/tab_dock_data.dart';
---
+++ z/tab/lib/src/tab_dock_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TabDockData {
+  List<Widget>? auxiliaryWidgets;
+  Widget? actionButton;
+
+  TabDockData({this.auxiliaryWidgets, this.actionButton});
+}
