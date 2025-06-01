3cd06eed202b27d1ffc8fbbe85cac559a611080c
Jango <me.jango@protonmail.com>
2025-06-01 11:20:59 -0300

    wip

--- z/lib/typedefs/lib/src/typedefs.dart
+++ z/lib/typedefs/lib/src/typedefs.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 typedef ChildrenBuilder = List<Widget> Function(BuildContext context);
 // TODO deprecate
-typedef ColorGetter = Color Function(BuildContext context);
+typedef ColorGetter = Color? Function(BuildContext context);
 // TODO deprecate
 typedef ContextPasser = void Function(BuildContext context);
 typedef EventListener = void Function();
